a22f90230c570f71addd0e289a3bb2a3f09aee5c
Chris Kaminski <ckpublic@ckaminski.com>
2024-03-12 21:19:21 -0400

    Bas working template with translation support

---
+++ i18n/QtExampleApp_en.ts
@@ -0,0 +1,45 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US" sourcelanguage="en_US">
+<context>
+    <name></name>
+    <message id="press-me">
+        <location filename="../content/Screen01.ui.qml" line="21"/>
+        <location filename="../../msvcx64-deb/qml/content/Screen01.ui.qml" line="21"/>
+        <source></source>
+        <translation type="unfinished">Please Press</translation>
+    </message>
+    <message id="hello-message">
+        <location filename="../content/Screen01.ui.qml" line="34"/>
+        <location filename="../../msvcx64-deb/qml/content/Screen01.ui.qml" line="34"/>
+        <source></source>
+        <translation type="unfinished">Hello World!</translation>
+    </message>
+    <message id="button-message">
+        <location filename="../content/Screen01.ui.qml" line="67"/>
+        <location filename="../../msvcx64-deb/qml/content/Screen01.ui.qml" line="67"/>
+        <source></source>
+        <translation type="unfinished">Button Pressed</translation>
+    </message>
+</context>
+<context>
+    <name>EventSimulator</name>
+    <message>
+        <location filename="../../msvcx64-deb/_deps/ds-src/src/imports/tools/eventsimulator/EventSimulator.qml" line="119"/>
+        <location filename="../../msvcx64-deb/_deps/ds-src/src/imports/tools/eventsimulator/EventSimulator.qml" line="144"/>
+        <location filename="../../msvcx64-deb/qml/QtQuick/Studio/EventSimulator/EventSimulator.qml" line="119"/>
+        <location filename="../../msvcx64-deb/qml/QtQuick/Studio/EventSimulator/EventSimulator.qml" line="144"/>
+        <source>Filter...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FlowDecision</name>
+    <message>
+        <location filename="../../msvcx64-deb/_deps/ds-src/src/imports/flowview/FlowDecision.qml" line="41"/>
+        <location filename="../../msvcx64-deb/qml/FlowView/FlowDecision.qml" line="41"/>
+        <source>Title</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
